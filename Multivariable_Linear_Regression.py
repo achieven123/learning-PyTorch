9396c05ecacf27185950336bc75350a89c21fcd6
achieven123 <achieven123@naver.com>
2023-05-09 22:30:59 +0900

    Update Multivariable_Linear_Regression.py

--- Multivariable_Linear_Regression.py
+++ Multivariable_Linear_Regression.py
@@ -5,12 +5,30 @@ import torch.optim as optim
 
 torch.manual_seed(1)
 
-x1_train = torch.FloatTensor([[73], [93], [89], [96], [73]])
-x2_train = torch.FloatTensor([[80], [88], [91], [98], [66]])
-x3_train = torch.FloatTensor([[75], [93], [90], [100], [70]])
+x_train = torch.FloatTensor([[73,  80,  75],
+                            [93,  88,  93],
+                            [89,  91,  80],
+                            [96,  98,  100],
+                            [73,  66,  70]])
 y_train = torch.FloatTensor([[152], [185], [180], [196], [142]])
 
-w1 = torch.zeros(1, requires_grad=True)
-w2 = torch.zeros(1, requires_grad=True)
-w3 = torch.zeros(1, requires_grad=True)
+W = torch.zeros((3, 1), requires_grad=True)
 b = torch.zeros(1, requires_grad=True)
+
+optimizer = optim.SGD([W, b], lr=1e-5)
+
+nb_epochs = 1000
+
+for epoch in range(nb_epochs + 1):
+    hypothesis = x_train * W + b
+
+    cost = torch.mean((hypothesis - y_train) ** 2)
+
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+    if epoch % 100 == 0:
+        print('Epoch {:4d}/{} hypothesis: {} Cost: {:.6f}'.format(
+            epoch, nb_epochs, hypothesis.squeeze().detach(), cost.item()
+        ))
