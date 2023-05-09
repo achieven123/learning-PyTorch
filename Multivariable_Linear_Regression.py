ff86e75897e8bb92c8d854a9217513fc234eae20
achieven123 <achieven123@naver.com>
2023-05-09 16:02:18 +0900

    Update

---
+++ Multivariable_Linear_Regression.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+torch.manual_seed(1)
+
+x1_train = torch.FloatTensor([[73], [93], [89], [96], [73]])
+x2_train = torch.FloatTensor([[80], [88], [91], [98], [66]])
+x3_train = torch.FloatTensor([[75], [93], [90], [100], [70]])
+y_train = torch.FloatTensor([[152], [185], [180], [196], [142]])
+
+w1 = torch.zeros(1, requires_grad=True)
+w2 = torch.zeros(1, requires_grad=True)
+w3 = torch.zeros(1, requires_grad=True)
+b = torch.zeros(1, requires_grad=True)
