a97e8012cc5a688c454bd903000966fe221508e8
achieven123 <achieven123@naver.com>
2023-05-23 02:46:59 +0900

    Update weight_and_bias.py

--- Deep_Learning_From_Scratch/Perceptron/weight_and_bias.py
+++ Deep_Learning_From_Scratch/Perceptron/weight_and_bias.py
@@ -50,15 +50,10 @@ def OR(x1, x2):
 
 
 def XOR(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([1.0, 1.0])
-    b = -0.5
-    tmp = np.sum(w * x) + b
-
-    if tmp <= 0:
-        return 0
-    else:
-        return 1
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
 
 
 print(XOR(1, 1))
