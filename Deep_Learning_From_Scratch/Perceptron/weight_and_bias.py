9c072b4d74f6a7fc89564db3428fd3db04e36fa7
achieven123 <achieven123@naver.com>
2023-05-23 00:53:27 +0900

    Update weight_and_bias.py

--- Deep_Learning_From_Scratch/Perceptron/weight_and_bias.py
+++ Deep_Learning_From_Scratch/Perceptron/weight_and_bias.py
@@ -1,10 +1,19 @@
 import numpy as np
 
 
-x = np.array([0, 1])
-w = np.array([0.5, 0.5])
-b = -0.7
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w * x) + b  # 넘파이에서 두 배열의 원소 수가 같다면, 각 원소끼리 곱함
 
-print(w * x)  # 넘파이에서 두 배열의 원소 수가 같다면, 각 원소끼리 곱함
-print(np.sum(w * x))
-print(np.sum(w * x) + b)
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+print(AND(0, 0))
+print(AND(0, 1))
+print(AND(1, 0))
+print(AND(1, 1))
