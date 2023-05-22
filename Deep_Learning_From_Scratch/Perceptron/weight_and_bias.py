c45583145f1de43f639b7405b643f3a5e5e32d1b
achieven123 <achieven123@naver.com>
2023-05-23 01:30:49 +0900

    Update weight_and_bias.py

--- Deep_Learning_From_Scratch/Perceptron/weight_and_bias.py
+++ Deep_Learning_From_Scratch/Perceptron/weight_and_bias.py
@@ -17,3 +17,48 @@ print(AND(0, 0))
 print(AND(0, 1))
 print(AND(1, 0))
 print(AND(1, 1))
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w * x) + b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+print(NAND(0, 0))
+print(NAND(0, 1))
+print(NAND(1, 0))
+print(NAND(1, 1))
+
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = 0.2
+    tmp = np.sum(w * x) + b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def XOR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([1.0, 1.0])
+    b = -0.5
+    tmp = np.sum(w * x) + b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+print(XOR(1, 1))
